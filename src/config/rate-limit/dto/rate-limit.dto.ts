5581686622f0c906ea71b5490eb0a6cea9005ea8
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-18 18:52:30 -0300

    feat: add RateLimitResult interface to define rate limit response structure

--- src/config/rate-limit/dto/rate-limit.dto.ts
+++ src/config/rate-limit/dto/rate-limit.dto.ts
@@ -1,3 +1,10 @@
 import { RATE_LIMIT_CONFIGS } from '@/config/rate-limit/constant/rate-limit.constant';
 
 export type RateLimitContext = keyof typeof RATE_LIMIT_CONFIGS;
+
+export interface RateLimitResult {
+  success: boolean;
+  limit: number;
+  remaining: number;
+  reset: number;
+}
