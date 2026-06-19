c4ce9aeed83a3581d0ff6cb410617f594cf610ef
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-18 22:27:40 -0300

    feat: add ErrorResponse interface to define error structure

--- src/common/errors/code/error.type.ts
+++ src/common/errors/code/error.type.ts
@@ -3,3 +3,7 @@ import { ERROR_CODE } from '@/common/errors/code/error.code';
 export type ErrorSeverity = 'expected' | 'unexpected';
 
 export type ErrorCode = (typeof ERROR_CODE)[keyof typeof ERROR_CODE];
+
+export interface ErrorResponse {
+  error: ErrorCode;
+}
